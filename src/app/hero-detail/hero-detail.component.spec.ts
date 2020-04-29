f4377a11cbc07c1206ef16059fd8592c2f75d8af
carlospalma <carlospalma03@gmail.com>
2020-04-29 18:51:06 -0500

    one more test for hero detail

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -12,13 +12,17 @@ import {
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
+  let heroSpy;
+  let heroSpyUpdate;
 
   beforeEach(async(() => {
     const spiderman:Hero ={id: 1,name:'spiderman' };
 
-    const heroService = jasmine.createSpyObj('HeroService', ['getHero']);
-    heroService.getHero.and.returnValue( of(spiderman) );
+    const heroService = jasmine.createSpyObj('HeroService', ['getHero','updateHero']);
+    heroSpy=heroService.getHero.and.returnValue( of(spiderman) );
+    
 
+    heroSpyUpdate = heroService.updateHero.and.returnValue(of(spiderman));
 
     TestBed.configureTestingModule({
       declarations: [ HeroDetailComponent ],
@@ -46,5 +50,12 @@ describe('HeroDetailComponent', () => {
 
   it('should get hero using id on url',() =>{
     expect(component.hero.name).toBe('spiderman');
+    expect(heroSpy.calls.any()).toBe(true);
   })
+
+  it('should update hero ',()=>{
+    component.save();
+    expect(heroSpyUpdate.calls.any()).toBe(true);
+  })
+
 });
