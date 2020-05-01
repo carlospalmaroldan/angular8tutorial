c9b956652ae2b38ebfa62f86276d1dfdb16462d4
carlospalma <carlospalma03@gmail.com>
2020-05-01 15:50:31 -0500

    adding e2e tests for hero search

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -22,6 +22,24 @@ describe('workspace-project App', () => {
     expect(element(by.tagName('app-root app-heroes')).all(by.tagName('a')).get(9).getText()).toBe('20 Tornado');
   });
 
+  it('should go to dashboard',() =>{
+    page.navigateTo();
+    element(by.linkText('Dashboard')).click();
+    expect(element(by.tagName('app-root app-dashboard')).all(by.tagName('a')).get(0).getText()).toBe('Narco');
+    expect(element(by.tagName('app-root app-dashboard')).all(by.tagName('a')).get(1).getText()).toBe('Bombasto');
+    expect(element(by.tagName('app-root app-dashboard')).all(by.tagName('a')).get(2).getText()).toBe('Celeritas');
+    expect(element(by.tagName('app-root app-dashboard')).all(by.tagName('a')).get(3).getText()).toBe('Magneta');
+  });
+
+  it('should search for a hero',()=>{
+    page.navigateTo;
+    element(by.id('search-box')).sendKeys('s');
+    
+    browser.sleep(1000);
+    expect( element(by.tagName('app-root app-dashboard app-hero-search')).all(by.tagName('a')).get(0).getText()).toBe('Bombasto');
+    expect( element(by.tagName('app-root app-dashboard app-hero-search')).all(by.className('search-result')).all(by.tagName('a')).get(1).getText()).toBe('Celeritas');
+  });
+
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
