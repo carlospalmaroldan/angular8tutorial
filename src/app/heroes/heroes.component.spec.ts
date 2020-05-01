43137e1748b2d2a3050603dc7ea52bcbe02c5d95
carlospalma <carlospalma03@gmail.com>
2020-05-01 13:04:07 -0500

    tests for heroes component, mocks heroes service

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -8,17 +8,19 @@ import { MessageService } from '../message.service';
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
-
+  const spiderman:Hero ={id: 1,name:'spiderman' };
   beforeEach(async(() => {
 
-    const spiderman:Hero ={id: 1,name:'spiderman' };
+    
     const batman: Hero = {id: 2,name:'batman'};
     const heroes=[spiderman,batman];
-
-    const heroService = jasmine.createSpyObj('HeroService', ['getHeroes']);
+    const talker: Hero={id:3,name:'talker'};
+    const heroService = jasmine.createSpyObj('HeroService', ['getHeroes','addHero','deleteHero']);
     const messageService = jasmine.createSpyObj('MessageService',['add']);
 
     heroService.getHeroes.and.returnValue( of(heroes) );
+    heroService.addHero.and.returnValue(of(talker));
+    heroService.deleteHero.and.returnValue(of(spiderman));
 
 
     TestBed.configureTestingModule({
@@ -46,4 +48,20 @@ describe('HeroesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should call add service',()=>{
+    expect(component.heroes.length).toBe(2);
+    const talker:Hero = {id:3,name:'talker'};
+    component.add('talker');
+    expect(component.heroes.length).toBe(3);
+  });
+
+  it('should call delete service',()=>{
+    expect(component.heroes.length).toBe(2);
+    component.delete(spiderman);
+    expect(component.heroes.length).toBe(1);
+
+  });
+
+
 });
